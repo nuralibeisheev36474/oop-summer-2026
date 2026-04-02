acf22d752fb8c2c782ba1f40c9230a794ef5e0c8
nuralibeisheev36474 <nuralibeisheev6@gmail.com>
2026-04-02 06:16:46 +0000

    Added lecture_4_test.py with variable, class, constant and comment

--- student/36474/lecture_4_test.py
+++ student/36474/lecture_4_test.py
@@ -1,1 +1,13 @@
+# Potential GitHub repository: "DeliciousBaklavaRepo" - Turkish tasty food
 
+# Variable with a long name (at least 3 words)
+number_of_students_in_class = 30
+
+# Class with a proper name
+class StudentProfile:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+# Constant with a proper name
+MAX_GRADE = 100
