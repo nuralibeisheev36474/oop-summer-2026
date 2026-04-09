b9d1f492a309f025e7ed18a17e9d23a7985c9ca3
nuralibeisheev36474 <nuralibeisheev6@gmail.com>
2026-04-09 08:38:31 +0000

    1

--- student/36474/test_06_class_with_constructor.py
+++ student/36474/test_06_class_with_constructor.py
@@ -1,1 +1,9 @@
+class Animal:
+    def __init__(self, name, size):
+        self.name = name
+        self.size = size
 
+my_pet = Animal("Rex", "Medium")    
+
+print(my_pet.name)
+print(my_pet.size)
--- student/36474/test_06_empty_class.py
+++ student/36474/test_06_empty_class.py
@@ -1,1 +1,9 @@
+class Animal:
+    pass
 
+my_pet = Animal()
+my_pet.name = "Aktosh"
+my_pet.size = "Big"
+
+print(my_pet.name)
+print(my_pet.size)
